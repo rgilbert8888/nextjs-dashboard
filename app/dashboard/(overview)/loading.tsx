8983ad055e462e9f714afef476784fe6becc8fc2
Robyn Gilbert <rgilbert@adhq.com>
2024-04-10 08:46:14 -0400

    Postgres, data fetching, streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+// Adding loading skeletons
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+// Streaming a whole page with loading.tsx
+
+// loading.tsx is a special Next.js file built on top of Suspense,
+// it allows you to create fallback UI to show as a replacement while page content loads.
+
+export default function Loading() {
+//   return <div>Loading...</div>;
+    return <DashboardSkeleton />;
+}
